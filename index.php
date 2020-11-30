ad10c683e969e2bfac3d95d64885682418dca346
Егор Креславский <kreslavskiy01@mail.ru>
2020-11-30 13:27:19 +0300

    laba2

--- index.php
+++ index.php
@@ -1,21 +1,45 @@
 <?php
-class A{
+class A
+{
+    public $x;
 
+    public function LinearEquation($a, $b){
+        if ($a == 0) {
+            throw new Exception('Division by zero');
+        }
+        return $this -> $x = (-$b) / $a;
+    }
 }
 
 class B extends A{
-    public function __construct($a1, $a2, $a3, $a4){
-        $this->a1 = $a1;
-        $this->a2 = $a2;
-        $this->a3 = $a3;
-        $this->a4 = $a4;
+    public function Discriminant($a, $b, $c){
+        return $this -> d = pow($b, 2) - 4 * $a * $c;
     }
+    public function QuadraticEquation($a, $b, $c){
+        if ($a == 0) {
+            return $this -> LinearEquation($a, $b);
+        }
+        $d = $this -> Discriminant($a, $b, $c);
 
-    protected $a1, $a2, $a3, $a4;
+        if ($d == 0) {
+            return $this -> x = [(-$b) / 2 * $a];
+        }
+        if ($d < 0) {
+            throw new Exception('It is impossible to solve the quadratic equation');
+        }
+        return $this -> x = [(-$b - sqrt($d)) / 2 * $a, (-$b + sqrt($d)) / 2 * $a];
+    }
 }
 
-$a1 = new A();
-$a2 = new A();
-$a3 = new A();
-$a4 = new A();
-$b = new B($a1, $a2, $a3, $a4);
+$a = new A();
+$b = new B($a);
+
+$linearResult = var_dump($a->solveLinearEquation(3, -4));
+$quadResult = var_dump($b->QuadraticEquation(3, -4, 4));
+$res = var_dump($b->QuadraticEquation(0, -4, 7));
+
+echo $linearResult;
+echo $quadResult;
+echo $res;
+
+
